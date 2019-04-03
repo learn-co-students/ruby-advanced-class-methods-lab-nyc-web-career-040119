82d19917fb18c21cceda5fd37716b4bab7f68bbe
JDM Ubuntu VB <JKDMarks@gmail.com>
2019-04-03 15:46:14 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,61 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |song|
+      song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    filename = filename.split(/[-.]/)
+    song = self.new_by_name(filename[1].strip)
+    song.artist_name = filename[0].strip
+    song
+  end
+
+  def self.create_from_filename(filename)
+    filename = filename.split(/[-.]/)
+    song = self.new_by_name(filename[1].strip)
+    song.artist_name = filename[0].strip
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
